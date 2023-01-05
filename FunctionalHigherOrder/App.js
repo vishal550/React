48adc760683854522fb64510a5fc52ddd43f6a01
code_for_vish <vishalpatil550@gmail.com>
2023-01-05 14:05:58 +0530

    Create App.js

---
+++ FunctionalHigherOrder/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Click from "./component/Click";
+import Mouse from "./component/Mouse";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Learning With Vishal</h1>
+      <Click />
+      <Mouse />
+    </div>
+  );
+}
+
+export default App;
